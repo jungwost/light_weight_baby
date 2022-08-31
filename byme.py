c5b5f48fd81c5c5ad2d5178c60448ea9b9d762d8
jungwost <79189703+jungwost@users.noreply.github.com>
2022-08-31 17:35:37 +0900

    Update byme.py

--- byme.py
+++ byme.py
@@ -1,5 +1,4 @@
 def sign():
     print(" 이 프로그램은 나도코딩에 의해 만들어졌습니다.\
-            \n유튜브 : http://youtube.com\
-            \ngit 테스트중임 ㅋ
+            \n유튜브 : http://youtube.co
             \n이메일 : nadocoding@gmail.com    ")
